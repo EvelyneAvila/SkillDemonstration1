1d8789148643e440886151d5d888a3c0688c920e
EvelyneAvila <emavila@ucsd.edu>
2022-02-04 13:35:50 -0800

    test - code fail

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,7 @@
 public class SkillDemo {
-  //VSCode
+  
+  //Method takesdimenstion of rectangle to get area
+  public static int RectangleArea(int b, int h){
+    return (b*h);
+  }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void AreaWithinTest() {
+        assertEquals(1, SkillDemo.RectangleArea(10, 5));
+    }
+}
